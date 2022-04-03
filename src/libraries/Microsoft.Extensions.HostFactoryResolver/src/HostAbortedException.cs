58af092d6a051e0b271c093239de9d5ebec8b009
Heath Baron-Morgan <heathbm@outlook.com>
2022-04-03 15:40:17 -0700

    Expose and update StopTheHostException to HostAbortedException

---
+++ src/libraries/Microsoft.Extensions.HostFactoryResolver/src/HostAbortedException.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Runtime.Serialization;
+
+namespace Microsoft.Extensions.Hosting
+{
+    [Serializable]
+    public sealed class HostAbortedException : Exception
+    {
+        public HostAbortedException() : base() { }
+        public HostAbortedException(string message) : base(message) { }
+        public HostAbortedException(string message, Exception innerException) : base(message, innerException) { }
+        private HostAbortedException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
